c2059b8e334796cc27e65ca4c8d79a815370045a
ikenna agulobi <ikennaww@yahoo.fr>
2020-04-18 21:39:45 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,9 +30,9 @@ def monopoly_with_third_tier
 
 {:railroads => 
        {
-  :pieces =>"4", 
+  :pieces =>4, 
   :rent_in_dollars => {
-    :one_piece_owned => 25 , :two_pieces_owned => "50" , :three_pieces_owned => "100", :foue_piece_owned => "200" } , 
+    :one_piece_owned => 25 , :two_pieces_owned => 50 , :three_pieces_owned => 100, :foue_pieces_owned => 200 } , 
   :names => {:reading_railroad => {} , :pennsylvania_railroad => {} , 
   :b_and_o_railroad => {}, :shortline_railroad => {}
     
@@ -58,10 +58,10 @@ def monopoly_with_fourth_tier
   } , 
   
   :names => {
-    :reading_railroad => {:mortgage_value => "100"} , 
-    :pennsylvania_railroad => {:mortgage_value => "200"} , 
-  :b_and_o_railroad => {:mortgage_value => "200"}, 
-  :shortline_railroad => {:mortgage_value => "800"}
+    :reading_railroad => {:mortgage_value => 100} , 
+    :pennsylvania_railroad => {:mortgage_value => 200} , 
+  :b_and_o_railroad => {:mortgage_value => 200}, 
+  :shortline_railroad => {:mortgage_value => 800}
     
   }
  }
