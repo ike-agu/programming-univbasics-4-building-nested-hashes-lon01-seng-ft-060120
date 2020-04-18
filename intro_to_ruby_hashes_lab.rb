abfe9cf398fb1fd0689b503191a195fd7a46b740
ikenna agulobi <ikennaww@yahoo.fr>
2020-04-18 21:28:16 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,10 +22,17 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
 
-{:railroads => {
+:railroads => {
   :pieces => "4" , 
-  :rent_in_dollars => {:one_piece_owned => "25" , } , 
-  :names => {}}}
+  :rent_in_dollars => {
+    :one_piece_owned => "25" , :two_piece_owned => "50" , :three_pice_owned => "100", :foue_piece_owned => "200" } , 
+  :names => {:reading_railroad => {} , :pennsylvania_railroad => {} , 
+  :b_and_o_railroad => {}, :shortline_railroad => {}
+    
+  }
+ }
+}
+
 
 
 end
@@ -35,5 +42,24 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+
+:railroads => {
+  :pieces => "4" , 
+  :rent_in_dollars => {
+    :one_piece_owned => "25" , :two_piece_owned => "50" , :three_pice_owned => "100", :foue_piece_owned => "200" 
+    
+  } , 
+  
+  :names => {
+    :reading_railroad => {:mortgage_value => "100"} , 
+    :pennsylvania_railroad => {:mortgage_value => "200"} , 
+  :b_and_o_railroad => {:mortgage_value => "200"}, 
+  :shortline_railroad => {:mortgage_value => "800"}
+    
+  }
+ }
+}
+
+
 
 end
