cdd2ea1402f997aa155bf2463cf69156d10f7a62
ikenna agulobi <ikennaww@yahoo.fr>
2020-04-18 20:44:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,6 +22,12 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
 
+{:railroads => {
+  :pieces => "4" , 
+  :rent_in_dollars => {:one_piece_owned => "25" , } , 
+  :names => {}}}
+
+
 end
 
 def monopoly_with_fourth_tier
