846c6a76de1fee42ed287f19486f798bc0c2cc41
ikenna agulobi <ikennaww@yahoo.fr>
2020-04-18 21:33:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,7 +12,7 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
 
-{:railroads => {:pieces => "4" , :rent_in_dollars => {} , :names => {}}}
+{:railroads => {:pieces =>"4" , :rent_in_dollars => {} , :names => {}}}
 
 end
 
@@ -22,8 +22,9 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
 
-{:railroads => {
-  :pieces => "4" , 
+{:railroads => 
+       {
+  :pieces =>"4", 
   :rent_in_dollars => {
     :one_piece_owned => "25" , :two_piece_owned => "50" , :three_pice_owned => "100", :foue_piece_owned => "200" } , 
   :names => {:reading_railroad => {} , :pennsylvania_railroad => {} , 
